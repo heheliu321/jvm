3ba93fc19639efc8d0fd50b7263b890a5fb4dcd3
heheliu321 <13777816569@163.com>
2020-11-01 16:43:24 +0800

    更新

---
+++ tuling/src/Math.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by Öî¸ğÀÏÊ¦ on 2018/9/26.
+ */
+class Math{
+	public int math() {
+		int a = 1; 
+		int b = 2;
+		int c = (a + b)*10;
+		return c;
+	}
+
+	public static void main(String[] args) {
+		Math math = new Math();
+		math.math();
+	}
+}
---
+++ tuling/src/TestGC.java
@@ -0,0 +1,26 @@
+ 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+import java.util.Random;
+
+public class TestGC {
+    public static void main(String[] args) throws Exception {
+        List<Object> list = new ArrayList<Object>();
+        while (true) {
+            int sleep = new Random().nextInt(100);
+            if (System.currentTimeMillis() % 2 == 0) {
+                list.clear();
+            } else {
+                for (int i = 0; i < 10000; i++) {
+                    Properties properties = new Properties();
+                    properties.put("key_" + i, "value_" +
+                        System.currentTimeMillis() + i);
+                    list.add(properties);
+                }
+            }
+// System.out.println("list大小为：" + list.size());
+            Thread.sleep(sleep);
+        }
+    }
+}
